a057861a351a7fc6958133b2437cd28bb57b44a0
digihax <mike.vasquez@gmail.com>
2022-02-10 09:53:25 -0700

    Update RustOxideUpdater.bat

--- RustOxideUpdater.bat
+++ RustOxideUpdater.bat
@@ -1,14 +1,18 @@
 :: This script brought to you by Digihax, aka The Professor, Admin of AZ Casual Rust Server
-:: Updated 2/9/2022
+:: Published 2/9/2022
+:: Updated 2/10/2022 - Initial local version check was not reliable.  Created an alternate version check
 
 :: CHANGE THIS to your local rust installation, with no trailing "\"
 Set RustPath=c:\Your\Rust\Root\Folder
 cd %RustPath%
 
+::Cleanup
+@del %RustPath%\*Oxide_Version.txt
+
 @echo on
 ::Grabbing the version of your local oxide install, and storing in root rust directory as Local_Oxide_Version.txt
 @echo Grabbing your installed Oxide version
-@powershell select-string %RustPath%\ConsoleOutput.txt -Pattern 'Oxide.Rust Version' -SimpleMatch | powershell -Command "$input | foreach-object {$_ -split ' '} | select -index 3 | Out-File %RustPath%\Local_Oxide_Version.txt"
+@powershell select-string %RustPath%\ConsoleOutput.txt -Pattern 'Loaded plugin Rust v' -SimpleMatch | powershell -Command "$input | foreach-object {$_ -split ' '} | select -index 4 | foreach-object {$_ -replace('v','')} | Out-File %RustPath%\Local_Oxide_Version.txt"
 
 @echo.
 @echo Your oxide version is: 
